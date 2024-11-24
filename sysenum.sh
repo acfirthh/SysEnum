d260e1ac1dbd85884e6581f02ee2864aac9f74d5
acfirth <59393271+acfirthh@users.noreply.github.com>
2024-11-24 22:26:43 +0000

    Update sysenum.sh

--- sysenum.sh
+++ sysenum.sh
@@ -1,70 +1,68 @@
 #!/bin/sh
 
+# Set colour variables
+Reset_Colour='\033[0m'
+Red='\033[1;31m'
+
 # Used to separate the output from each command
-SEPARATOR="\n\e[1;31m--------------------------------------------------------------------------------\e[0m"
+SEPARATOR="--------------------------------------------------------------------------------"
 
 # Gets the username of the current user
-echo "$SEPARATOR\n\e[1;33;4mUsername:\e[0m\n$(whoami)"
+printf "${Red}$SEPARATOR${Reset_Colour}\n Username:\n$(whoami)\n"
 
 # Gets the hostname of the machine
-echo "$SEPARATOR\n\e[1;33;4mHostname:\e[0m\n$(hostname)"
+printf "${Red}$SEPARATOR${Reset_Colour}\n Hostname:\n$(hostname)\n"
 
 # Lists the kernel version of the Linux that is running on the machine
-echo "$SEPARATOR\n\e[1;33;4mKernel:\e[0m\n$(uname -a)"
+printf "${Red}$SEPARATOR${Reset_Colour}\n Kernel:\n$(uname -a)\n"
 
 # Gets the Linux version
-echo "$SEPARATOR\n\e[1;33;4mLinux Version:\e[0m\n$(lsb_release -a)"
+printf "${Red}$SEPARATOR${Reset_Colour}\n Linux Version:\n$(lsb_release -a)\n"
 
 # Gets the kernel version and compiler version that is installed
-echo "$SEPARATOR\n\e[1;33;4mKernel Version and Compilers:\e[0m\n$(cat /proc/version)"
+printf "${Red}$SEPARATOR${Reset_Colour}\n Kernel Version and Compilers:\n$(cat /proc/version)\n"
 
 # Lists the processes for the current shell
-echo "$SEPARATOR\n\e[1;33;4mProcesses for current shell:\e[0m\n$(ps)"
+printf "${Red}$SEPARATOR${Reset_Colour}\n Processes for current shell:\n$(ps)\n"
 
 # Lists all processes currently running for the current user
-echo "$SEPARATOR\n\e[1;33;4mAll Processes:\e[0m\n$(ps -A)"
-
-# Shows the process tree
-echo "$SEPARATOR\n\e[1;33;4mProcess Tree:\e[0m\n$(ps axjf)"
-
-# Shows all processes currently running for ALL users
-echo "$SEPARATOR\n\e[1;33;4mProcesses for all users:\e[0m\n$(ps aux)"
+printf "${Red}$SEPARATOR${Reset_Colour}\n All Processes:\n$(ps -A)\n"
 
 # Shows environment variables
-echo "$SEPARATOR\n\e[1;33;4mEnvironment Variables:\e[0m\n$(env)"
+printf "${Red}$SEPARATOR${Reset_Colour}\n Environment Variables:\n$(env)\n"
 
 # Shows the system path variable
-echo "$SEPARATOR\n\e[1;33;4mSystem Path:\e[0m\n$PATH"
+printf "${Red}$SEPARATOR${Reset_Colour}\n System Path:\n$PATH\n"
 
 # Shows the current working directory
-echo "$SEPARATOR\n\e[1;33;4mCurrent Directory:\e[0m\n$(pwd)"
+printf "${Red}$SEPARATOR${Reset_Colour}\n Current Directory:\n$(pwd)\n"
 
 # Shows all files in the current working directory, including hidden files
-echo "$SEPARATOR\n\e[1;33;4mAll Files in Current Directory:\e[0m\n$(ls -la)"
+printf "${Red}$SEPARATOR${Reset_Colour}\n All Files in Current Directory:\n$(ls -la)\n"
 
 # Shows the user privilege levels
-echo "$SEPARATOR\n\e[1;33;4mUser's Privilege Level:\e[0m\n$(id)"
+printf "${Red}$SEPARATOR${Reset_Colour}\n User's Privilege Level:\n$(id)\n"
 
 # Shows the contents of the /etc/passwd file
-echo "$SEPARATOR\n\e[1;33;4m/etc/passwd Contents:\e[0m\n$(cat /etc/passwd)"
+printf "${Red}$SEPARATOR${Reset_Colour}\n /etc/passwd Contents:\n$(cat /etc/passwd)\n"
 
 # Shows the 'real' users that have a /home/<username> directory
-echo "$SEPARATOR\n\e[1;33;4mReal Users:\e[0m\n$(cat /etc/passwd | grep home)"
+printf "${Red}$SEPARATOR${Reset_Colour}\n Real Users:\n$(cat /etc/passwd | grep home)\n"
 
 # Shows the network interfaces
-echo "$SEPARATOR\n\e[1;33;4mNetwork Interfaces:\e[0m\n$(ifconfig)"
+printf "${Red}$SEPARATOR${Reset_Colour}\n Network Interfaces:\n$(ifconfig)\n"
 
 # Shows mounted devices
-echo "$SEPARATOR\n\e[1;33;4mMounted Devices:\e[0m\n$(df -h)"
+printf "${Red}$SEPARATOR${Reset_Colour}\n Mounted Devices:\n$(lsblk)\n"
 
 # Shows the contents of the /etc/hosts file
-echo "$SEPARATOR\n\e[1;33;4mHosts File Contents:\e[0m\n$(cat /etc/hosts)"
+printf "${Red}$SEPARATOR${Reset_Colour}\n Hosts File Contents:\n$(cat /etc/hosts)\n"
 
 # Gets all current listening ports 
-echo "$SEPARATOR\n\e[1;33;4mAll Listening Ports:\e[0m\n$(netstat -a)"
+printf "${Red}$SEPARATOR${Reset_Colour}\n All Listening Ports:\n$(netstat -a)\n"
 
 # Lists the current connections including their PID
-echo "$SEPARATOR\n\e[1;33;4mList Connections with PID:\e[0m\n$(netstat -tp)"
+printf "${Red}$SEPARATOR${Reset_Colour}\n List Connections with PID:\n$(netstat -tp)\n"
 
 # Shows all files with an SUID bit that can be run with higher privileges than the current user
-echo "$SEPARATOR\n\e[1;35;4mShow files with SUID bit:\e[0m\n$(find / -perm -u=s -type f 2>/dev/null)$SEPARATOR"
+printf "${Red}$SEPARATOR${Reset_Colour}\n Show files with SUID bit:\n$(find / -perm -u=s -type f 2>/dev/null)\n"
